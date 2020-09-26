480a07ba221c9ce0b195b563a36fd342f6f5310c
Adisken <38098134+Adisken@users.noreply.github.com>
2020-09-26 10:55:19 +0200

    Add files via upload

---
+++ Obiektowe Kolokwium/PawlaczykPawel/Nauczyciel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawlaczykPawel
+{
+    class Nauczyciel : Pracownik
+    {
+        private string przedmiot { get; set; }
+
+        public void Info()
+        {
+            Console.WriteLine("Nauczyciel: ");
+            base.Info();
+            Console.WriteLine($"Przedmiot: {this.przedmiot}");
+        }
+        public Nauczyciel(string name, string surname, string topic)
+        {
+            imie = name;
+            nazwisko = surname; //mozliwosc poprawy potem czy cos
+            przedmiot = topic;
+
+        }
+    }
+}
